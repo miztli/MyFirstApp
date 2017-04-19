ff06bf705d2923066007bce382cf93c1f6eec44e
Miztli Melgoza <dark_coneja@hotmail.com>
2017-04-19 09:18:42 -0500

    Add Hello World message and first Activity

--- app/src/main/java/com/example/miztli/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/miztli/myfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.miztli.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Changing a text wiew from a button
+        //TextView textView = (TextView) findViewById(R.id.textView_welcome);
+        //textView.setText(R.string.change_message);
     }
 }
